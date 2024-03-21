b51e2e40eefdde8de9bafc3076d82b884e4a01f0
DanielCapannini <daniel.capannini@studio.unibo.it>
2024-03-21 18:12:11 +0100

    update Task and TaskTest

--- src/main/scala/u03/Task.scala
+++ src/main/scala/u03/Task.scala
@@ -4,73 +4,75 @@ import Sequences.*
 import Sequence.*
 import Optionals.*
 import Streams.*
+import Stream.*
 import u02.AlgebraicDataTypes.IntList
 import u02.Modules.isStudent
 import u02.Modules.Person
 import u02.Modules
 
-object Task extends App:
-
+object Task:
     //part 1
-
+  extension [A](l: Sequence[A])
     //1
-    def take[A](l: Sequence[A], n: Int): Sequence[A] = (l, n) match
-        case (_, 0) | (Nil(), _) => Nil()
-        case (Cons(head, tail), _) => Cons(head, take(tail, n - 1))
+    def take(n: Int): Sequence[A] = (l, n) match
+      case (_, 0) | (Nil(), _) => Nil()        
+      case (Cons(head, tail), _) => Cons(head, tail.take(n - 1))
 
-    def zip[A,B](l: Sequence[A], r: Sequence[B]): Sequence[(A,B)] = (l, r) match
-        case (_, Nil()) | (Nil(), _) => Nil()
-        case (Cons(head1, tail1), Cons(head2, tail2)) => Cons((head1, head2), zip(tail1, tail2))
+    def zip[B](r: Sequence[B]): Sequence[(A,B)] = (l, r) match
+      case (_, Nil()) | (Nil(), _) => Nil()
+      case (Cons(head1, tail1), Cons(head2, tail2)) => Cons((head1, head2), tail1.zip(tail2))
 
-    def concat[A](l: Sequence[A], r: Sequence[A]): Sequence[A] = (l, r) match
-        case (Nil(), _) => r
-        case (Cons(head, tail), _) => Cons( head, concat(tail, r))
+    def concat(r: Sequence[A]): Sequence[A] = (l, r) match
+      case (Nil(), _) => r
+      case (Cons(head, tail), _) => Cons( head, tail.concat(r))
 
-    def flatMap[A,B](l: Sequence[A])(f: A=> Sequence[B]): Sequence[B] = l  match
-        case (Nil()) => Nil()
-        case (Cons(head, tail)) => concat(f(head), flatMap(tail)(f))
+    def flatMap[B](f: A=> Sequence[B]): Sequence[B] = l  match
+      case (Nil()) => Nil()
+      case (Cons(head, tail)) => f(head).concat(tail.flatMap(f))
 
-    def map[A, B](l: Sequence[A])(f: A => B): Sequence[B] = flatMap(l)(a => Cons(f(a), Nil()))
+    def map[B](f: A => B): Sequence[B] = l.flatMap(a => Cons(f(a), Nil()))
 
-    def filter[A](l: Sequence[A])(pred: A => Boolean): Sequence[A] = flatMap(l)(a => if (pred(a)) Cons(a, Nil()) else Nil())
+    def filter(pred: A => Boolean): Sequence[A] = l.flatMap(a => if (pred(a)) Cons(a, Nil()) else Nil())
+  
 
     //2
-    def min(l: Sequence[Int]): Optional[Int] = l match
-        case (Cons(head, tail)) if min(filter(tail)(_<head)) != Optional.Empty() => min(filter(tail)(_<head))
-        case (Cons(head, tail)) if min(filter(tail)(_<head)) == Optional.Empty() => Optional.Just(head)
-        case (Nil()) => Optional.Empty()
-        case (Cons(_, _)) => Optional.Empty()
+  def min(l: Sequence[Int]): Optional[Int] = l match
+    case Cons(head1, Cons(head2, tail)) if head1 > head2 => min(Cons(head2, tail))
+    case Cons(head1, Cons(head2, tail)) if head1 <= head2 => min(Cons(head1, tail))
+    case Cons(head, Nil()) => Optional.Just(head)
+    case Nil() => Optional.Empty()
+    
 
     //part 2
 
     //3
-    def getCourses(s: Sequence[Modules.Person]): Sequence[String] = 
-        flatMap(s):
-            case Modules.Person.Teacher(_, c) => Cons(c, Nil())
-            case _ => Nil()
+  def courses(s: Sequence[Modules.Person]): Sequence[String] = 
+    s.flatMap:
+      case Modules.Person.Teacher(_, c) => Cons(c, Nil())
+      case _ => Nil()
     
     //4
-    def foldLeft(s: Sequence[Int])(x: Int)(f: (Int, Int) => Int): Int = s match
-        case (Nil()) => x
-        case (Cons(head, tail)) => foldLeft(tail)(f(x, head))(f)
+  def foldLeft(s: Sequence[Int])(x: Int)(f: (Int, Int) => Int): Int = s match
+    case (Nil()) => x
+    case (Cons(head, tail)) => foldLeft(tail)(f(x, head))(f)
 
     //part 3
-
-    import Stream.*
-
-    //6
+ 
+    //6 
+    /*
     def takeWhile[A](stream: Stream[A])(pred: A => Boolean): Stream[A] = stream match
-      case Cons(head, tail) if pred(head()) => cons(head(), takeWhile(tail())(pred))
-      case _ => Empty()
+      case Cons(head, tail) if pred(head()) => cons(head(), takeWhile(tail)(pred))
+      case _ => empty()
+    */
 
     //7
-    def fill[A](n: Int)(k: A): Stream[A] = n match
-      case n if n>0 => cons(k, fill(n-1)(k))
-      case _ => empty()
+  def fill[A](n: Int)(k: A): Stream[A] = n match
+    case n if n>0 => cons(k, fill(n-1)(k))
+    case _ => empty()
 
     //8
-    val pell: Stream[Int] = {
-      def pellHelper(a: Int, b: Int): Stream[Int] =
-        cons(a, pellHelper(b, 2 * b + a))
-      pellHelper(0, 1)
-    }
+  val pell: Stream[Int] = {
+    def pellHelper(a: Int, b: Int): Stream[Int] =
+      cons(a, pellHelper(b, 2 * b + a))
+    pellHelper(0, 1)
+  }
--- src/test/scala/u03/TaskTest.scala
+++ src/test/scala/u03/TaskTest.scala
@@ -9,53 +9,59 @@ import Stream.*
 import u03.Sequences.*
 import Sequence.*
 import u02.Modules.Person
-import u02.AlgebraicDataTypes.Person
+import u03.Optionals.*
+import u03.Optionals.Optional.*
+
 
 class TaskTest:
 
-  @Test def testTake() =
+  @Test def testTake(): Unit =
     val l: Sequence[Int] = Cons(10, Cons(20, Cons(30, Nil())))
-    assertEquals(Cons(10, Cons(20, Nil())), take(l)(2))
-    assertEquals(Cons(10, Cons(20, Cons(30, Nil()))), take(l)(3))
-    assertEquals(Nil(), take(l)(0))
-    assertEquals(Nil(), take(Nil())(2))
+    assertEquals(Cons(10, Cons(20, Nil())), l.take(2))
+    assertEquals(Cons(10, Cons(20, Cons(30, Nil()))), l.take(3))
+    assertEquals(Nil(), l.take(0))
+    assertEquals(Nil(), Nil().take(2))
   
-  @Test def testZip() = 
+  @Test def testZip(): Unit =
     val l: Sequence[Int] = Cons(10, Cons(20, Cons(30, Nil())))
     val l2: Sequence[String] = Cons("10", Cons("20", Cons("30", Nil())))
-    assertEquals(Cons((10, "10"), Cons((20, "20"), Cons((30, "30"), Nil()))), zip(l, l2))
-    assertEquals(Nil(), zip(l, Nil()))
-    assertEquals(Nil(), zip(Nil(), l2))
-    assertEquals(Nil(), zip(Nil(), Nil()))
+    assertEquals(Cons((10, "10"), Cons((20, "20"), Cons((30, "30"), Nil()))), l.zip(l2))
+    assertEquals(Nil(), l.zip(Nil()))
+    assertEquals(Nil(), Nil().zip(l2))
+    assertEquals(Nil(), Nil()zip(Nil()))
 
-  @Test def testConcat() =
-    val l2: Sequence[Int] = Cons(40, Cons(50, Nil()))
-    assertEquals(Cons(10, Cons(20, Cons(30, Cons(40, Cons(50, Nil()))))), concat(l, l2))
-    assertEquals(Cons(40, Cons(50, Nil())), concat(Nil(), l2))
-
-  @Test def testFlatMap() =
-    assertEquals(Cons(11, Cons(21, Cons(31, Nil()))), flatMap(l)(v => Cons(v + 1, Nil())))
-    assertEquals(Nil(), flatMap(Nil())(v => Cons(v, Nil())))
-
-  @Test def testMap() =
-    assertEquals(Cons(11, Cons(21, Cons(31, Nil()))), map(l)(_ + 1))
-    assertEquals(Cons("10", Cons("20", Cons("30", Nil()))), map(l)(_ + ""))
-
-  @Test def testFilter() =
-    assertEquals(Cons(20, Cons(30, Nil())), filter(l)(_ >= 20))
-    assertEquals(Cons(10, Cons(30, Nil())), filter(l)(_ != 20))
-  
-  @Test def testMin() =
+  @Test def testConcat(): Unit =
     val l: Sequence[Int] = Cons(10, Cons(20, Cons(30, Nil())))
-    assertEquals(Just(10), min(l))
-    assertEquals(Just(1), min(Cons(1, Nil())))
-    assertEquals(Empty(), min(Nil()))
+    val l2: Sequence[Int] = Cons(40, Cons(50, Nil()))
+    assertEquals(Cons(10, Cons(20, Cons(30, Cons(40, Cons(50, Nil()))))), l.concat(l2))
+    assertEquals(Cons(40, Cons(50, Nil())), Nil().concat(l2))
 
-  @Test def testGetCorses() =
+  @Test def testFlatMap(): Unit =
+    val l: Sequence[Int] = Cons(10, Cons(20, Cons(30, Nil())))
+    assertEquals(Cons(11, Cons(21, Cons(31, Nil()))), l.flatMap(v => Cons(v + 1, Nil())))
+    assertEquals(Nil(), Nil().flatMap(v => Cons(v, Nil())))
+
+  @Test def testMap(): Unit =
+    val l: Sequence[Int] = Cons(10, Cons(20, Cons(30, Nil())))
+    assertEquals(Cons(11, Cons(21, Cons(31, Nil()))), l.map(_ + 1))
+    assertEquals(Cons("10", Cons("20", Cons("30", Nil()))), l.map(_ + ""))
+
+  @Test def testFilter(): Unit =
+    val l: Sequence[Int] = Cons(10, Cons(20, Cons(30, Nil())))
+    assertEquals(Cons(20, Cons(30, Nil())), l.filter(_ >= 20))
+    assertEquals(Cons(10, Cons(30, Nil())), l.filter(_ != 20))
+  
+  @Test def testMin(): Unit =
+    val l: Sequence[Int] = Cons(10, Cons(20, Cons(30, Nil())))
+    assertEquals(Just(10), Task.min(l))
+    assertEquals(Just(1), Task.min(Cons(1, Nil())))
+    assertEquals(Empty(), Task.min(Nil()))
+
+  @Test def testGetCorses(): Unit =
     val l3: Sequence[Person] = Cons(Person.Teacher("Viroli", "PPS"), Cons(Person.Teacher("Ghini", "SO"), Nil()))
-    assertEquals(Cons("PPS", Cons("SO", Nil())), getCourses(l3))
+    assertEquals(Cons("PPS", Cons("SO", Nil())), courses(l3))
 
-  @Test def testFoldLeft() =
+  @Test def testFoldLeft(): Unit =
     val lst = Cons(3,Cons(7,Cons(1,Cons(5, Nil()))))
     assertEquals(-16, foldLeft(lst)(0)(_- _))
 
@@ -64,6 +70,10 @@ class TaskTest:
     val str2 = Stream.takeWhile(str1)(_ < 5) // {0,1,2,3,4}
     assertEquals(Cons(0, Cons(1, Cons(2, Cons(3, Cons(4, Nil()))))), Stream.toList(str2))
 
-  @Test def testFill() =
-    assertEquals(Cons(a, Cons(a, Cons(a, Nil()))), Stream.toList(Stream.fill(3)("a")))
-    
+  @Test def testFill(): Unit =
+    assertEquals(Cons("a", Cons("a", Cons("a", Nil()))), Stream.toList(Stream.fill(3)("a")))
+
+  @Test def testPell(): Unit =
+    val l = Cons(0, Cons(1, Cons(2, Cons(5, Cons(12, Nil())))))
+    assertEquals(Cons(0, Nil()), Stream.toList(Stream.take(pell)(1)))
+    assertEquals(l, Stream.toList(Stream.take(pell)(5)))
